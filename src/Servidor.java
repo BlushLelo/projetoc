6b8365ad697f02878debf423491cbd098eda8719
Lucas Simões <ssl.lucas.simoes@gmail.com>
2020-05-01 16:23:30 -0300

    Some comments

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,3 +1,17 @@
+/*
+* Usar MySQL - 1 tabela apenas contendo informaçãoes do desenho.
+* Colunas da tabela:.
+* Nome: nome do desenho.
+* IP: ip do cliente para identificação
+* data: data de criação do desenho (criada quando o desenho for criado).
+* modificado: data de ultima modificação do desenho.
+* conteudo: conteudo do desenho (text).
+*
+* - Se o nome ja existir o desenho deve ser sobreescrito no banco, caso não exista cria-se um registro.
+* - Uso de Threads para aceitar vários clientes. (1 threads por cliente)
+* -
+* */
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
